58ff5df60145c8e52c249c45dd956400cee292b6
Ali Binkowska <midnightskyuniverse@gmail.com>
2025-12-05 14:45:20 +0100

    analytics & cookies

--- scripts/build-assets.js
+++ scripts/build-assets.js
@@ -2,4 +2,21 @@
 
 const renderAssets = require('./render-assets');
 
+const fs = require('fs');
+const path = require('path');
+
+// Your existing asset copying code...
+
+// Copy sitemap.xml
+if (fs.existsSync('public/sitemap.xml')) {
+    fs.copyFileSync('public/sitemap.xml', 'dist/sitemap.xml');
+    console.log('Copied sitemap.xml');
+}
+
+// Copy robots.txt
+if (fs.existsSync('public/robots.txt')) {
+    fs.copyFileSync('public/robots.txt', 'dist/robots.txt');
+    console.log('Copied robots.txt');
+}
+
 renderAssets();
